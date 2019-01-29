423ed35abf79684e8488afb29e604e11157cb519
JohnTorrance8270 <johnabertay8270@hotmail.com>
2019-01-29 08:56:31 +0000

    Inital commit

---
+++ FirstTaskResults/src/StaticPractice/Main.java
@@ -0,0 +1,11 @@
+package StaticPractice;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		StaticController staticControl = new StaticController();
+		staticControl.run();
+		StaticController.methodStatic();
+	}
+}
